afd6b159dd1409255bcbc6c92d0ca81bc362a22e
Ulrich Koffi <koffiulrich538@gmail.com>
2026-02-21 23:53:23 +0000

    fix lucide-react

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "https://zando-backend.onrender.com/api/",
   headers: {
     "Content-Type": "application/json",
   },
