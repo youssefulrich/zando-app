2129e4162411976eeb65e2682f6542d298aa5c66
Ulrich Koffi <koffiulrich538@gmail.com>
2026-03-03 16:57:25 +0000

    Add event image support

--- apps/events/admin.py
+++ apps/events/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from .models import Event, TicketType, Ticket
 
-# Register your models here.
+
+class TicketTypeInline(admin.TabularInline):
+    model = TicketType
+    extra = 1
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ("title", "category", "location", "event_date", "image_preview")
+    inlines = [TicketTypeInline]
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html(
+                '<img src="{}" width="80" height="80" style="border-radius:8px;" />',
+                obj.image.url
+            )
+        return "-"
+    
+    image_preview.short_description = "Preview"
